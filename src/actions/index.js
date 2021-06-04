f4f054d105a3778e6454040fed727962da77d3e6
tarunj210 <54388330+tarunj210@users.noreply.github.com>
2021-06-04 16:19:06 +0530

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,33 @@
+import { actionTypes } from './actionTypes';
 
+
+export const increment = () => {
+    return function (dispatch) {
+        dispatch({
+            type: actionTypes.INCREMENT_COUNTER,
+        });
+    }
+}
+
+export const decrement = () => {
+    return function (dispatch) {
+        dispatch({
+            type: actionTypes.DECREMENT_COUNTER,
+        });
+    }
+}
+export const reset = () => {
+    return function (dispatch) {
+        dispatch({
+            type: actionTypes.RESET_COUNTER,
+        });
+    }
+}
+export const changeCountTo = (numberChange) => {
+    return function (dispatch) {
+        dispatch({
+            type: actionTypes.CHANGE_COUNT,
+            payload: numberChange
+        });
+    }
+}
